d8749d14added067e61e6699b8cb6d90707b233b
ida ms <119769726+idamsufiana@users.noreply.github.com>
2023-08-27 20:15:17 +0700

    Add files via upload

---
+++ LeetcodeProblems/ReverseInteger/ReverseInteger.java
@@ -0,0 +1,31 @@
+public class ReverseInteger {
+
+    static int reverse(int num) {
+        int rev = 0  ;
+ 
+    while(num != 0){       
+      int rem = num % 10 ;
+      num /= 10 ;
+ 
+      if(rev > Integer.MAX_VALUE/10 || rev == Integer.MAX_VALUE/10 && rem > 7){
+        return 0 ;
+      }
+ 
+      if(rev < Integer.MIN_VALUE/10 || rev == Integer.MIN_VALUE/10 && rem < -8){
+        return 0 ;
+      }
+ 
+      rev = rev*10 + rem ;
+    }
+ 
+    return rev ;
+    }
+
+    public static void main(String[] args)
+    {
+        int num = 123;
+     
+        System.out.print( reverse(num) );
+    }
+    
+}
